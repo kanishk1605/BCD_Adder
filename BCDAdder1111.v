763e2dcf2816299f499762633c33481e2bf49446
Kanishk Agarwal <105859363+kanishk1605@users.noreply.github.com>
2024-01-10 22:34:21 +0530

    Update BCDAdder1111.v

--- BCDAdder1111.v
+++ BCDAdder1111.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.01.2024 17:52:03
-// Design Name: 
-// Module Name: BCDAdder1111
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module BCDAdder1111;
    
